65df80525a7db1eb7f59693d0a90cee2016d0892
devcom33 <mouad.elb06@gmail.com>
2024-03-31 05:00:46 -0700

    add comments

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-
+"""given employee ID, returns information about his/her TODO list progress"""
 import requests
 import sys
 
 userId = sys.argv[1]
-user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
-name = user.json().get('name')
-todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
+    name = user.json().get('name')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-total = 0
-completed = 0
-for i in todos.json():
-    if i.get('userId') == int(userId):
-        total += 1
-        if i.get('completed') is True:
-            completed += 1
-print("Employee {} is done with tasks({}/{}):".format(name, completed, total))
+    total = 0
+    completed = 0
+    for i in todos.json():
+        if i.get('userId') == int(userId):
+            total += 1
+            if i.get('completed') is True:
+                completed += 1
+    print("Employee {} is done with tasks({}/{}):".format(name, completed, total))
 
-for task in todos.json():
-    if task.get('userId') == int(userId):
-        if task.get('completed') is True:
-            print("     {}".format(task.get('title')))
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            if task.get('completed') is True:
+                print("     {}".format(task.get('title')))
