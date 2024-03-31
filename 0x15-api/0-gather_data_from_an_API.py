58252dace3476481a9665a158639168022fadeb1
devcom33 <mouad.elb06@gmail.com>
2024-03-31 05:40:22 -0700

    upd

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -26,4 +26,4 @@ if __name__ == "__main__":
           .format(name, completed, total))
 
     print('\n'.join(["\t " + task.get('title') for task in todos.json()
-        if task.get('userId') == int(userId) and task.get('completed')]))
+          if task.get('userId') == int(userId) and task.get('completed')]))
