a54fe4e0215c28be0353a7c12083b5908022f684
devcom33 <mouad.elb06@gmail.com>
2024-03-31 04:52:34 -0700

    add tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,6 @@ import sys
 userId = sys.argv[1]
 user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
 name = user.json().get('name')
-print("-----------------------------------------------")
 todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
 total = 0
@@ -17,3 +16,8 @@ for i in todos.json():
         if i.get('completed') is True:
             completed += 1
 print("Employee {} is done with tasks({}/{}):".format(name, completed, total))
+
+for task in todos.json():
+    if task.get('userId') == int(userId):
+        if task.get('completed') is True:
+            print("     {}".format(task.get('title')))
