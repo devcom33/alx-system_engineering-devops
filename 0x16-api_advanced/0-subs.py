5d489599d1cbc9da6c64fb22c8cf71f12963b50c
devcom33 <mouad.elb06@gmail.com>
2024-08-13 14:52:10 +0000

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-	response = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers, allow_redirects=False)
+	headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0'}
+	response = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers)
 	
-	if response.status_code == 404:
+	if response.status_code != 200:
 		return 0
-	results = response.json().get("data")
-	
-	return results.get("subscribers")
 
+	try:
+		results = response.json()
+		return results.get("data", {}).get("subscribers", 0)
+	except ValueError:
+		return 0 
